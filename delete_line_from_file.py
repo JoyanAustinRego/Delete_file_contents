c24350fe1feee20f912062d10031158e62a73030
joyanaustinrego <joyanaustinrego@gmail.com>
2019-11-16 12:58:38 +0530

    functionality done, tested

--- delete_line_from_file.py
+++ delete_line_from_file.py
@@ -1,21 +1,35 @@
 import os
 import re
 
+
+master_contents_list = []
+string_contents_list = []
+
+
 with open("master_file.txt", "r") as master_file:
     master_contents = master_file.readlines()
-
+    
 
 with open("string_file.txt", "r") as string_file:
     string_contents = string_file.readlines()
 
 
-for master_element in master_contents:   
-    for string_element in string_contents:
+for master_elements in master_contents:
+    master_contents_list.append(master_elements.strip('\n'))
+
+
+for string_element in string_contents:
+    string_contents_list.append(string_element.strip('\n'))
+
+ 
+for master_element in master_contents_list:   
+    for string_element in string_contents_list:
        if(re.search(string_element, master_element)):
-           master_contents.remove(master_element)
+           master_contents_list.remove(master_element)
 
          
 with open("master_file.txt", "w") as master_file:
-    for master_element in master_contents:
-        master_file.writelines(master_element)
+    for master_element in master_contents_list:
+        master_file.writelines(master_element+"\n")
       
+
